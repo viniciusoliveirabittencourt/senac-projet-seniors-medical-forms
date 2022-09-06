e79f04af388951dc3c2599a209e7eef4583544c0
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2022-09-06 14:45:34 -0300

    é o desespero irmão

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    mySecret: 'secret',
+    secondSecret: process.env.SECOND_SECRET, // Pass through env variables
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    staticFolder: '/static',
+  },
   async header() {
     return [
       {
