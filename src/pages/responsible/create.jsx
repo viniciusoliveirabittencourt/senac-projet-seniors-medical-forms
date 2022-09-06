76ecef5d80ba573dc61b30e005c634b3873cb746
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2022-09-06 13:21:38 -0300

    aaa

--- src/pages/responsible/create.jsx
+++ src/pages/responsible/create.jsx
@@ -40,7 +40,7 @@ export default function Home() {
         ...generalForm,
         mySeniors: []
       }
-      await axios.post('/api/createResponsible', body, { headers: { 'Content-Type': 'multipart/form-data', 'cache': { maxAge: 2 * 60 * 100, exclude: { filter: req => getURLCached(req), query: false } } } });
+      await axios.post('/api/createResponsible', body, { headers: { 'Content-Type': 'multipart/form-data', 'Cache-Control': 'no-cache' } });
       router.push('/')
       setCarregando(false);
     } catch (e) {
