cf8c96b48939c305b4135931951753d0cada85bc
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2022-09-06 11:38:17 -0300

    new branch

--- src/database/upload.ts
+++ src/database/upload.ts
@@ -4,9 +4,9 @@ import multerS3 from 'multer-s3';
 import crypto from 'crypto';
 
 aws.config.update({
-  secretAccessKey: process.env.AWS_SECRET_KEY,
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  region: process.env.AWS_REGION,
+  secretAccessKey: process.env.AWS_SECRET_KEY_VERCEL,
+  accessKeyId: process.env.AWS_ACCESS_KEY_VERCEL,
+  region: process.env.AWS_REGION_VERCEL,
 });
 
 const s3 = new aws.S3({  });
@@ -14,7 +14,7 @@ const s3 = new aws.S3({  });
 const upload = multer({
   storage: multerS3({
     s3,
-    bucket: process.env.AWS_BUCKET,
+    bucket: process.env.AWS_BUCKET_VERCEL,
     acl: 'public-read',
     contentType: multerS3.AUTO_CONTENT_TYPE,
     metadata: function (req, file, cb) {
